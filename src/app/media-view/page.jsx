4479df5d03767ab2cd09a9f37572794ab4b841d1
Waledilson <95579186+Waledilson@users.noreply.github.com>
2024-07-11 17:19:35 -0300

    more font size bullshit

--- src/app/media-view/page.jsx
+++ src/app/media-view/page.jsx
@@ -18,9 +18,13 @@ function MediaView() {
 
       <div className="youtube">
         <Suspense fallback={<p>Loading video...</p>}>
+          <p>Corporate Kid</p>
           <YoutubeEmbed embedId={`gC2TWLJXvJw?si=Wug6Cvp7FKivsUJx`} />
+          <p>Echo and Fade</p>
           <YoutubeEmbed embedId={`nb9_bH3sENE?si=MVkE4QwLsJBdTaQy`} />
+          <p>Tragically Hip - Grace, too</p>
           <YoutubeEmbed embedId={`WFiG9Z2QvLE?si=BOA1_zdw5YqguDhY`} />
+          <p>The Weatherman (pt. 2)</p>
           <YoutubeEmbed embedId={`_g69FegBvwA?si=EjGaL13jVvH_d9BW`} />
         </Suspense>
       </div>

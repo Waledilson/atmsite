c3e126589b327ee594ace5c979b1d6cd1de1c244
Waledilson <95579186+Waledilson@users.noreply.github.com>
2024-07-12 00:52:10 -0300

    ..

--- src/app/media-view/page.jsx
+++ src/app/media-view/page.jsx
@@ -5,46 +5,38 @@ import YoutubeEmbed from "../ui/youtube-embed.jsx"
 function MediaView() {
   return (
     <div className="media-view">
-      <div>
-        <h1 className="media-title">MEDIA</h1>
-        <div className="streaming-platforms">
-          <a
-            href="https://open.spotify.com/artist/21ha0CPxFe19DAiE4aKdgs?si=T8mZUrKvQa2kSlTYPvenhA"
-            target="_blank"
-          >
-            <img className="spotify-logo" src="/img/spotify-logo.svg" />
-          </a>
-          <a
-            href="https://music.apple.com/ca/artist/alert-the-medic/318287737"
-            target="_blank"
-          >
-            <img className="apple-logo" src="/img/apple-music-logo.svg" />
-          </a>
-          <a
-            href="https://amazon.com/music/player/artists/B002BINUTW/alert-the-medic?marketplaceId=ATVPDKIKX0DER&musicTerritory=US&ref=dm_sh_qNw0nfZoOZsFF0QeFoAnZDPDG"
-            target="_blank"
-          >
-            <img className="amazon-logo" src="/img/Amazon_Music_White.png" />
-          </a>
-          <a href="https://deezer.page.link/hkSN3PRJZVire5z69" target="_blank">
-            <img className="deezer-logo" src="/img/deezer-logo.svg" />
-          </a>
-          <a href="https://tidal.com/browse/artist/5531956" target="_blank">
-            <img className="tidal-logo" src="/img/tidal-logo.png" />
-          </a>
-          <a
-            href="https://youtube.com/@alertthemedic?si=ubFXQpfI89sunC8A"
-            target="_blank"
-          >
-            <img className="youtube-logo" src="/img/YouTube-logo.svg" />
-          </a>
-
-          {/* <p>spotify</p>
-          <p>amazon</p>
-          <p>apple music</p>
-          <p>deezer</p>
-          <p>tidal</p> */}
-        </div>
+      <h1 className="media-title">MEDIA</h1>
+      <div className="streaming-platforms">
+        <a
+          href="https://open.spotify.com/artist/21ha0CPxFe19DAiE4aKdgs?si=T8mZUrKvQa2kSlTYPvenhA"
+          target="_blank"
+        >
+          <img className="spotify-logo" src="/img/spotify-logo.svg" />
+        </a>
+        <a
+          href="https://music.apple.com/ca/artist/alert-the-medic/318287737"
+          target="_blank"
+        >
+          <img className="apple-logo" src="/img/apple-music-logo.svg" />
+        </a>
+        <a
+          href="https://amazon.com/music/player/artists/B002BINUTW/alert-the-medic?marketplaceId=ATVPDKIKX0DER&musicTerritory=US&ref=dm_sh_qNw0nfZoOZsFF0QeFoAnZDPDG"
+          target="_blank"
+        >
+          <img className="amazon-logo" src="/img/Amazon_Music_White.png" />
+        </a>
+        <a href="https://deezer.page.link/hkSN3PRJZVire5z69" target="_blank">
+          <img className="deezer-logo" src="/img/deezer-logo.svg" />
+        </a>
+        <a href="https://tidal.com/browse/artist/5531956" target="_blank">
+          <img className="tidal-logo" src="/img/tidal-logo.png" />
+        </a>
+        <a
+          href="https://youtube.com/@alertthemedic?si=ubFXQpfI89sunC8A"
+          target="_blank"
+        >
+          <img className="youtube-logo" src="/img/YouTube-logo.svg" />
+        </a>
       </div>
 
       <div className="youtube">
